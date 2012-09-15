3e06608bddbe5a3bf2001b56f8dbfaba4765c971
Gabriel Grant <g@briel.ca>
2012-09-14 22:55:26 -0700

    initial import

---
+++ README.md
@@ -0,0 +1,15 @@
+Precompile Handlebars templates for Ember.js
+
+Install
+-------
+
+    npm install -g ember-precompile
+
+Usage
+-----
+
+This module has a similar interface to the Handlebars precompiler
+
+    ember-precompile template... [-f OUTPUT_FILE]
+
+If output file is omitted, the compiled template will be printed to stdout.
---
+++ index.js
@@ -0,0 +1,65 @@
+var fs = require('fs');
+var path = require('path');
+var vm = require('vm');
+
+var HANDLEBARSJS = fs.readFileSync(__dirname + '/vendor/handlebars-1.0.0.beta.6.js', 'utf8')
+var EMBERJS = fs.readFileSync(__dirname + '/vendor/ember-1.0.pre.min.js', 'utf8')
+
+module.exports = function (file) {
+  //dummy jQuery
+  var jQuery = function () { return jQuery }
+  jQuery.ready = function () { return jQuery }
+  jQuery.inArray = function () { return jQuery }
+  jQuery.jquery = "1.7.1"
+  jQuery.event = { fixHooks: {} }
+
+  //dummy DOM element
+  var element = {
+    firstChild: function () { return element },
+    innerHTML: function () { return element }
+  }
+
+  var sandbox = {
+    // DOM
+    document: {
+      createRange: false,
+      createElement: function () { return element }
+    },
+
+    // Console
+    console: console,
+
+    // jQuery
+    jQuery: jQuery,
+    $: jQuery,
+
+    // handlebars template to compile
+    template: fs.readFileSync(file, 'utf8'),
+
+    // compiled handlebars template
+    templatejs: null
+  }
+
+  // window
+  sandbox.window = sandbox
+
+  // create a context for the vm using the sandbox data
+  var context = vm.createContext(sandbox)
+
+  // load Handlebars and Ember into the sandbox
+  vm.runInContext(HANDLEBARSJS, context, 'ember.js')
+  vm.runInContext(EMBERJS, context, 'ember.js')
+
+  // compile the handlebars template inside the vm context
+  vm.runInContext('templatejs = Ember.Handlebars.precompile(template).toString()', context)
+
+  // extract the compiled template from the vm context and return it,
+  // adding template to Ember.TEMPLATES when it is required
+  var fileName = path.basename(file)
+  var namedTemplateJs = 'Ember.TEMPLATES["' +
+    fileName.replace(/.handlebars/, '') +
+    '"] = Ember.Handlebars.template(' + context.templatejs + ');'
+
+  return namedTemplateJs;
+}
+
