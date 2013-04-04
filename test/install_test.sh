2ed7a44b538b3c720f2aa33e0faeb165f5a89864
Gabriel Grant <g@briel.ca>
2013-04-04 12:07:43 -0700

    don't install all dev requirements when testing: it takes forever

--- test/install_test.sh
+++ test/install_test.sh
@@ -8,7 +8,7 @@ PACKAGE_DIR=$(pwd)
 
 mkdir "$TEST_INSTALL_DIR"
 pushd  "$TEST_INSTALL_DIR"
-npm install --dev "$PACKAGE_DIR"
+npm install "$PACKAGE_DIR"
 
 # smoke test
 ./node_modules/.bin/ember-precompile "$PACKAGE_DIR/test/test1.hbs"
