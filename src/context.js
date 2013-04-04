b511ecde2bc52335bcfaf7a9e8e856e04ce2fb50
Gabriel Grant <g@briel.ca>
2013-04-04 12:02:10 -0700

    upgrade to ember-1.0-rc2; refactor context creation

---
+++ src/context.js
@@ -0,0 +1,78 @@
+//
+// Sets up the context needed to use Ember with Node
+//
+
+var fs = require('fs');
+var vm = require('vm');
+
+var HANDLEBARSJS = fs.readFileSync(__dirname + '/../vendor/handlebars-1.0.rc.3.js', 'utf8')
+//var EMBERJS = fs.readFileSync(__dirname + '/../vendor/ember-1.0.pre.min.js', 'utf8')
+var EMBERJS = fs.readFileSync(__dirname + '/../vendor/ember-1.0.0-rc.2.js', 'utf8')
+
+function getBaseSandbox() {
+
+  //dummy jQuery
+  var jQuery = function () { return jQuery }
+  jQuery.ready = function () { return jQuery }
+  jQuery.inArray = function () { return jQuery }
+  jQuery.jquery = "1.8.1"
+  jQuery.event = { fixHooks: {} }
+
+  //dummy DOM element
+  var element = {
+    firstChild: function () { return element },
+    innerHTML: function () { return element },
+    // needed for "movesWhitespace()" test
+    childNodes: [
+      {nodeValue: 'Test: '},
+      {nodeValue: ''},
+      {nodeValue: 'Value'}
+    ]
+  }
+
+  var sandbox = {
+    // DOM
+    document: {
+      createRange: false,
+      createElement: function () { return element }
+    },
+
+    // Console
+    console: console,
+
+    // jQuery
+    jQuery: jQuery,
+    $: jQuery,
+
+    // setTimeout is needed by Ember's run loop
+    setTimeout: setTimeout
+  }
+
+  // window
+  sandbox.window = sandbox;
+
+  return sandbox;
+}
+
+function getContext(sandboxExtras){
+  sandbox = getBaseSandbox();
+  sandboxExtras = sandboxExtras || {};
+  for (var attrname in sandboxExtras){
+    sandbox[attrname] = sandboxExtras[attrname];
+  }
+  
+  // create a context for the vm using the sandbox data
+  var context = vm.createContext(sandbox)
+
+  // load Handlebars and Ember into the sandbox
+  vm.runInContext(HANDLEBARSJS, context, 'ember.js')
+  vm.runInContext(EMBERJS, context, 'ember.js')
+  return context;
+}
+
+module.exports = {
+  getContext: getContext,
+  getBaseSandbox: getBaseSandbox
+};
+
+// vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
---
+++ src/index.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var path = require('path');
+var vm = require('vm');
+var context_ = require('./context');
+
+module.exports = function (file) {
+  var context = context_.getContext({
+    // handlebars template to compile
+    template: fs.readFileSync(file, 'utf8'),
+
+    // compiled handlebars template
+    templatejs: null
+  });
+
+  // compile the handlebars template inside the vm context
+  vm.runInContext('templatejs = Ember.Handlebars.precompile(template).toString()', context)
+
+  // extract the compiled template from the vm context and return it,
+  // adding template to Ember.TEMPLATES when it is required
+  var fileName = path.basename(file)
+  var namedTemplateJs = 'Ember.TEMPLATES["' +
+    fileName.replace(/.handlebars/, '').replace(/.hbs/, '') +
+    '"] = Ember.Handlebars.template(' + context.templatejs + ');'
+
+  return namedTemplateJs;
+}
+
+module.exports.context = context_;
