acdce0f66ab94cd890559c644f11662125259634
Gabriel Grant <g@briel.ca>
2013-04-04 12:04:28 -0700

    add simple smoke test (install and run)
    fixes #10

---
+++ test/install_test.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# run this from the top of the node-ember-precompile repo
+
+TEST_INSTALL_DIR="/tmp/ember-precompile-test-$(date +%s)"
+
+PACKAGE_DIR=$(pwd)
+
+mkdir "$TEST_INSTALL_DIR"
+pushd  "$TEST_INSTALL_DIR"
+npm install --dev "$PACKAGE_DIR"
+
+# smoke test
+./node_modules/.bin/ember-precompile "$PACKAGE_DIR/test/test1.hbs"
+
+popd
+
+
